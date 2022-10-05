c8ba3013fcaab833898f7a772cdef780812574b4
Patrick Gu <patrickthegu@gmail.com>
2022-10-05 22:49:47 +1100

    more changes to js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,8 +15,8 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('putDb implemented');
-  const contactDb = await openDB('jate', 1);
-  const tx = contactDb.transaction('jate', "readwrite")
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
@@ -27,8 +27,8 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error('getDb implemented');
-  const contactDb = await openDB('jate', 1);
-  const tx = contactDb.transaction('jate', "readonly")
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
